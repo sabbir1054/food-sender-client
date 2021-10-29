3fc3384f692c07ea895dbd0191055d068d9560b5
Your Name <you@example.com>
2021-10-29 07:48:50 +0600

    firebase setup

---
+++ src/Firebase/AuthInit.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./Firebase.config";
+
+
+const AuthInitialization = () => {
+    initializeApp(firebaseConfig);
+}
+
+export default AuthInitialization;
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzgkktb02DimxbEHTHP_399g1smTa9NDQ",
+
+  authDomain: "food-sender-muhammadsabbir.firebaseapp.com",
+
+  projectId: "food-sender-muhammadsabbir",
+
+  storageBucket: "food-sender-muhammadsabbir.appspot.com",
+
+  messagingSenderId: "577978728213",
+
+  appId: "1:577978728213:web:cc4755eb3d586bd5f0731c",
+};
+
+export default firebaseConfig;
