4c50557766c05d6dcefd14fbe4d409b69b934ada
Your Name <you@example.com>
2021-10-30 20:30:13 +0600

    multiple and single oreder updated

--- src/Pages/MyOrder/MyOrder.js
+++ src/Pages/MyOrder/MyOrder.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
-const MyOrder = () => {
-    return (
-        <div>
-            <h1>Single order</h1>
-        </div>
+const MyOrder = (props) => {
+    const {email,name,foodId,condition } = props.order;
+    return email ? (
+      <tr>
+        <td>{name}</td>
+        <td>{email}</td>
+        <td>{foodId}</td>
+        <td>{condition}</td>
+        <td>
+          <button className="btn btn-danger">Cancel Order</button>
+        </td>
+      </tr>
+    ) : (
+      <div className="d-flex justify-content-center align-items-center">
+        <Spinner animation="border" role="status "></Spinner>
+      </div>
     );
 };
 
--- src/Pages/MyOrder/MyOrders.js
+++ src/Pages/MyOrder/MyOrders.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Table } from 'react-bootstrap';
+import useAuth from '../../Hooks/useAuth';
 import PageBanner from '../PageBanner/PageBanner';
 import MyOrder from './MyOrder';
 
 const MyOrders = () => {
-    return (
-        <div>
-            <PageBanner text='Your Order Condition'></PageBanner>
-            <MyOrder></MyOrder>
+  const { user } = useAuth();
+  const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+      fetch(`http://localhost:5000/userOrders/${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setOrders(data));
+    }, []);
+    console.log(orders);
+  return (
+    <div>
+      <PageBanner text="Your Order Condition"></PageBanner>
+      <Container>
+        <div className="py-5">
+          <Table striped bordered hover responsive="md">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Food Id</th>
+                <th>Order Condition</th>
+                <th>Manage Order</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                  <MyOrder order={order}></MyOrder>
+                ))}
+            </tbody>
+          </Table>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default MyOrders;
